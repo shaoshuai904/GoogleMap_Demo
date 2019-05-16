e2d25eedb03316dde9a6a48f829865ca5dfbe6d2
Maple Shao <939078792@qq.com>
2019-05-16 19:36:44 +0800

    update app utils.

--- app/src/main/java/com/maple/googlemap/utils/AppUtils.kt
+++ app/src/main/java/com/maple/googlemap/utils/AppUtils.kt
@@ -1,6 +1,5 @@
 package com.maple.googlemap.utils
 
-import android.annotation.TargetApi
 import android.content.Context
 import android.content.pm.PackageInfo
 import android.content.pm.PackageManager
@@ -20,41 +19,24 @@ object AppUtils {
     fun getAppVersionString(context: Context): String {
         val packageInfo = getPackageInfo(context)
         var verName = "1.0"
-        var verCode = 1
+        var verCode = 1L
         if (packageInfo != null) {
             verName = packageInfo.versionName
-            verCode = packageInfo.versionCode
+            verCode = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                packageInfo.longVersionCode
+            } else {
+                packageInfo.versionCode.toLong()
+            }
         }
         return "Version $verName - (Build $verCode)"
     }
 
     /**
-     * APP版本名
-     */
-    fun getVersionName(context: Context): String {
-        val packageInfo = getPackageInfo(context)
-        return if (packageInfo != null) packageInfo.versionName else "1.0"
-    }
-
-    /**
-     * APP版本号
-     */
-    fun getVersionCode(context: Context): Int {
-        val packageInfo = getPackageInfo(context)
-        return packageInfo?.versionCode ?: 1
-    }
-
-    /**
      * 获取APP包信息
      */
-    @TargetApi(Build.VERSION_CODES.N)
-    fun getPackageInfo(context: Context): PackageInfo? {
+    private fun getPackageInfo(context: Context): PackageInfo? {
         val pm = context.packageManager
-        // PackageInfo pi = pm.getPackageInfo(context.getPackageName(),
-        // PackageManager.GET_CONFIGURATIONS);
-        // PackageInfo pi = pm.getPackageInfo(context.getPackageName(), 0);
-        return pm.getPackageInfo(context.packageName, PackageManager.MATCH_UNINSTALLED_PACKAGES)
-
+        return pm.getPackageInfo(context.packageName, PackageManager.GET_CONFIGURATIONS)
     }
 
 }
